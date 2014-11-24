41cb71fd2b99ab531b9a7f5c53c6161fa908aed9
Claessens Simon <gagalago@gmail.com>
2014-11-24 12:23:59 +0100

    ACTION_UID_METHOD_NAME for model change

--- lib/eventually_tracker/core_ext.rb
+++ lib/eventually_tracker/core_ext.rb
@@ -5,6 +5,11 @@ module EventuallyTracker
     def self.extend_active_record_base(eventually_tracker)
       ActiveRecord::Base.class_eval do
         define_singleton_method :track_change do
+          before_save do
+            action_uid              = SecureRandom.hex
+            @eventually_action_uid  = action_uid
+            ActiveRecord::Base.send(:define_method, ACTION_UID_METHOD_NAME, proc { action_uid })
+          end
           after_save do
             model_name  = self.class.name.underscore
             created     = created_at == updated_at
@@ -12,13 +17,15 @@ module EventuallyTracker
             action_uid  = send(ACTION_UID_METHOD_NAME)
             eventually_tracker.track_change model_name, created, action_uid, data
           end
+          after_save do
+            ActiveRecord::Base.send(:remove_method, ACTION_UID_METHOD_NAME)
+          end
         end
       end
     end
 
     def self.extend_active_controller_base(eventually_tracker)
       ActionController::Base.class_eval do
-
         define_singleton_method :track_action do | options = {} |
           before_action do
             action_uid              = SecureRandom.hex
