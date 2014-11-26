4ddb104d4e7e93b8b0b6b1f5678e1e5e9cd43dca
Loïc Vigneron <loic.vigneron@gmail.com>
2014-11-26 22:44:38 +0100

    Extract track_change method

--- lib/eventually_tracker/core_ext.rb
+++ lib/eventually_tracker/core_ext.rb
@@ -4,60 +4,29 @@ module EventuallyTracker
     ACTION_UID_METHOD_NAME = "eventually_tracker_action_uid"
 
     def self.extend_active_record_base_dummy
-      ActiveRecord::Base.class_eval do
-        define_singleton_method :track_change do | options = {} |
-        end
-      end
+      ActiveRecord::Base.class_eval { define_singleton_method :track_change }
     end
 
     def self.extend_active_record_base(eventually_tracker)
       ActiveRecord::Base.class_eval do
-        define_singleton_method :track_change do | options = {} |
-          after_create do
-            track_change eventually_tracker, :create
-          end
-
-          after_update do
-            track_change eventually_tracker, :update
-          end
-
-          after_destroy do
-            track_change eventually_tracker, :destroy
-          end
-
-          def track_change(eventually_tracker, action_name)
-            model_name  = self.class.name.underscore
-            data        = changes.except :created_at, :updated_at
-            data        = { id: [id, nil] } if action_name == :destroy
-            action_uid  = send(ACTION_UID_METHOD_NAME) if self.respond_to?(ACTION_UID_METHOD_NAME)
-            eventually_tracker.track_change model_name, action_name, action_uid, data
-          end
+        define_singleton_method :track_change do
+          after_create  { EventuallyTracker::CoreExt.track_change eventually_tracker, :create, changes }
+          after_update  { EventuallyTracker::CoreExt.track_change eventually_tracker, :update, changes }
+          after_destroy { EventuallyTracker::CoreExt.track_change eventually_tracker, :destroy, changes }
         end
       end
     end
 
     def self.extend_active_controller_base_dummy
-      ActionController::Base.class_eval do
-        define_singleton_method :track_action do | options = {} |
-        end
-      end
+      ActionController::Base.class_eval { define_singleton_method :track_action }
     end
 
     def self.extend_active_controller_base(eventually_tracker)
       ActionController::Base.class_eval do
         define_singleton_method :track_action do | options = {} |
-
-          before_action options do
-            EventuallyTracker::CoreExt.define_action_uid
-          end
-
-          before_action options do
-            EventuallyTracker::CoreExt.track_action eventually_tracker, params, session
-          end
-
-          after_action options do
-            EventuallyTracker::CoreExt.remove_action_uid
-          end
+          before_action(options) { EventuallyTracker::CoreExt.define_action_uid }
+          before_action(options) { EventuallyTracker::CoreExt.track_action eventually_tracker, params, session }
+          after_action(options) { EventuallyTracker::CoreExt.remove_action_uid }
         end
       end
     end
@@ -80,6 +49,14 @@ module EventuallyTracker
       action_name     = params[:action]
       data            = params.except :controller, :action
       eventually_tracker.track_action controller_name, action_name, @eventually_action_uid, data, session_data
+    end
+
+    def self.track_change(eventually_tracker, action_name, changes)
+      model_name  = self.class.name.underscore
+      data        = changes.except :created_at, :updated_at
+      data        = { id: [id, nil] } if action_name == :destroy
+      action_uid  = send(ACTION_UID_METHOD_NAME) if self.respond_to?(ACTION_UID_METHOD_NAME)
+      eventually_tracker.track_change model_name, action_name, action_uid, data
     end
 
     def self.extract_tracked_session_keys(keys, session)
